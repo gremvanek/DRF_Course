0a04a1d77aabc2d182434651b9bcc1c5efadfe03
gremvanek <gremvanek@gmail.com>
2024-05-28 23:00:51 +0500

    Миграции.

--- habits/urls.py
+++ habits/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from habits.views import HabitListAPIView
 
+app_name = 'habits'
 
 urlpatterns = [
     path("habits/", HabitListAPIView.as_view(), name="habit-list"),
